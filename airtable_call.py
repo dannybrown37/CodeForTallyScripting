5b891a04bdca5e9d0111ed77a1884eeac1501ffb
Danny Brown <dannybrown37@gmail.com>
2019-04-18 21:04:26 -0400

    basic card layout running

---
+++ airtable_call.py
@@ -0,0 +1,53 @@
+# requires
+# pip install airtable
+# pip install airtable-python-wrapper
+
+import json
+import airtable
+from airtable import Airtable
+
+BASE_ID = "app38cZA2uPDxdTL8" # found in url of API documentation for table
+TABLE_NAME = "Services"
+
+def airtable_call():
+    airtable = Airtable(BASE_ID, TABLE_NAME)
+    records = airtable.get_all()
+
+    service_list = []
+    for record in records:
+        od = {} # Original Dictionary
+        od['icons'] = []
+        try:
+            od['name'] = record['fields']['Name']
+        except KeyError:
+            pass
+        try:
+            od['desc'] = record['fields']['Desc']
+        except KeyError:
+            pass
+        try:
+            for index, record_id in enumerate(record['fields']['Icons']):
+                record = airtable.get(record_id)
+                od['icons'].append({
+                    "text" : "",
+                    "icon" : ""
+                })
+                try:
+                    od['icons'][index]['text'] = record['fields']['text']
+                except KeyError:
+                    pass
+                try:
+                    od['icons'][index]['icon'] = record['fields']['icon']
+                except KeyError:
+                    pass
+        except KeyError:
+            pass
+        if od['icons'] == []:
+            del od['icons']
+
+        service_list.append(od)
+
+    with open("output.json", "w") as f:
+        json.dump(service_list, f, indent=2)
+
+    return service_list
---
+++ cards.py
@@ -0,0 +1,19 @@
+import json
+from flask import Flask, render_template
+from airtable_call import airtable_call
+
+
+# CARDS_PER_ROW = 3
+# let's worry about grouping/rows in the front-end using CSS.
+# This way we can be responsive to screen size without having to rewrite
+# back-end logic
+
+app = Flask(__name__)
+
+@app.route('/cards', methods=['GET'])
+def hello(city='Tallahassee'):
+    cards = airtable_call()
+    return render_template('cards.html', cards=cards)
+
+if __name__ == '__main__':
+    app.run(debug=True)
