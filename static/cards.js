fea5ac4833c99e5ebbd8f3c871e2cf5248d9e023
Danny Brown <dannybrown37@gmail.com>
2019-07-19 14:45:25 -0400

    cleaned up JS and CSS, added pseudoclasses to card links for color

--- static/cards.js
+++ static/cards.js
@@ -1,11 +1,15 @@
 $(document).ready(function(){
 
-  // jQuery to handle action with #card-resizer button is clicked
+  // jQuery to handle action when #card-resizer button is clicked
   $('#flip-button').click(function() {
     $("#flip-button .flip-card-inner").toggleClass("big-cards-active");
-    $("#cards").toggleClass("big-cards");
-    $("#cards").toggleClass("little-cards");
+    $("#cards").toggleClass("big-cards").toggleClass("little-cards");
     $(".flip-card").toggleClass("big-card");
+  });
+
+  // jQuery to make grid sortable - https://jqueryui.com/sortable/#display-grid
+  $(function () {
+    $("#cards").sortable().disableSelection();
   });
 
 });
