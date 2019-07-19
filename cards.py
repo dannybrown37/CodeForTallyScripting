ad6de6933b8d6e5aeff5b3025146d967d620809f
Danny Brown <dannybrown37@gmail.com>
2019-07-19 14:46:34 -0400

    removed unneeded comment

--- cards.py
+++ cards.py
@@ -3,11 +3,6 @@ from flask import Flask, render_template
 from modules.airtable_call import airtable_call
 
 
-# CARDS_PER_ROW = 3
-# let's worry about grouping/rows in the front-end using CSS.
-# This way we can be responsive to screen size without having to rewrite
-# back-end logic
-
 app = Flask(__name__)
 
 @app.route('/cards', methods=['GET'])
